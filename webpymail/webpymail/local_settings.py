47e8a7a52a82459cf19510f2f89e4743f0a46685
hguerreiro <hguerreiro@71cd05b2-794b-0410-99d5-6df88e3db98d>
2013-08-22 00:53:39 +0000

    Removed unwanted file

--- webpymail/webpymail/local_settings.py
+++
@@ -1,9 +0,0 @@
-import os.path
-
-CONFIGDIR = os.path.join('/home/helder/prg/webpymail-config')
-USERCONFDIR = os.path.join(CONFIGDIR, 'users')
-SERVERCONFDIR = os.path.join(CONFIGDIR, 'servers')
-FACTORYCONF = os.path.join(CONFIGDIR,'factory.conf')
-DEFAULTCONF = os.path.join(CONFIGDIR,'defaults.conf')
-SERVERCONF  = os.path.join(CONFIGDIR,'servers.conf')
-SYSTEMCONF  = os.path.join(CONFIGDIR,'system.conf')
